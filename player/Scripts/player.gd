1c1db954af2eb229149126f520bf6fdc58c3fa3e
AqlanNaqib <aqlannaqib2@gmail.com>
2025-04-22 23:05:32 +0800

    Revert "player movement (idle&walk animation)"

--- player/Scripts/player.gd
+++ player/Scripts/player.gd
@@ -1,64 +1,23 @@
-class_name ExamplePlayer extends CharacterBody2D
+class_name Player extends CharacterBody2D
 
-var cardinal_direction : Vector2 = Vector2.DOWN
-var direction : Vector2 = Vector2.ZERO
-var move_speed : float = 100
-var state : String = "idle"
+var move_speed : float = 100.0
 
-@onready var animation_player : AnimationPlayer = $AnimationPlayer
-@onready var sprite : Sprite2D = $ExamplePlayerSprite
 
 # Called when the node enters the scene tree for the first time.
-func _ready(): 
+func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process( delta ):
-	
+# Called every frame. 'Delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction : Vector2 = Vector2.ZERO
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
 	velocity = direction * move_speed
-	 
-	if SetState() == true:
-		UpdateAnimation()
-
-
+	
 	pass
 
-func _physics_process( delta ):
+
+func _physics_process(delta):
 	move_and_slide()
- 
-
-
-
-
-func SetDirection() -> bool:
-	
-	return true
-	
-	
-	
-func SetState() -> bool:
-	var new_state : String = "idle" if direction == Vector2.ZERO else "walk"
-	if new_state == state:
-		return false
-	state = new_state
-	return true
-	
-	
-	
-func UpdateAnimation() -> void:
-	animation_player.play( state + "_" + AnimDirection() )
-	pass
-	
-
-
-func AnimDirection() -> String:
-	if cardinal_direction == Vector2.DOWN:
-		return "down"
-	elif cardinal_direction == Vector2.UP:
-		return "up"
-	else:
-		return "side"
