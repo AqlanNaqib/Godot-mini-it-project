1014b0fa3c6ab4d7ef61c1e2d2b2f36b71971cd1
EzraGGIt <ezra2766@gmail.com>
2025-04-26 17:14:04 +0800

    Main character done
    the basic of main character is done but the coding need to be change for the player movement

--- player/Scripts/player.gd
+++ player/Scripts/player.gd
@@ -1,23 +1,45 @@
 class_name Player extends CharacterBody2D
 
+var cardinal_direction : Vector2 = Vector2.DOWN
+var direction : Vector2 = Vector2.ZERO
 var move_speed : float = 100.0
+var state : String = "idle"
 
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var sprite : Sprite2D = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'Delta' is the elapsed time since the previous frame.
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var direction : Vector2 = Vector2.ZERO
+	
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-	
+
 	velocity = direction * move_speed
-	
-	pass
-
-
-func _physics_process(delta):
 	move_and_slide()
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
