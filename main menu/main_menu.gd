4b863dbe4dccd59a400f656368ffe4e8eae04557
Aqlan <aqlannaqib2@gmail.com>
2025-05-03 21:03:56 +0800

    sound effect click

--- main menu/main_menu.gd
+++ main menu/main_menu.gd
@@ -6,7 +6,9 @@ extends Control
 @onready var options_button = $MarginContainer/HBoxContainer/VBoxContainer/Options_Button as Button
 @onready var options_menu = $Options_Menu as OptionMenu
 @onready var margin_container = $MarginContainer as MarginContainer
+@onready var sfx_click: AudioStreamPlayer2D = $sfx_click
 
+ 
 
 
 func _ready():
@@ -14,6 +16,7 @@ func _ready():
 
 
 func on_start_pressed() -> void:
+	sfx_click.play()
 	pass
 
 
@@ -21,16 +24,19 @@ func on_options_pressed() -> void:
 	margin_container.visible = false
 	options_menu.set_process(true)
 	options_menu.visible = true
+	sfx_click.play()
 	
 
 
 func on_exit_pressed() -> void: 
 	get_tree().quit() 
+	sfx_click.play()
 
 
 func on_exit_options_menu() -> void:
 	margin_container.visible = true
 	options_menu.visible = false
+	sfx_click.play()
 	
 	
 
