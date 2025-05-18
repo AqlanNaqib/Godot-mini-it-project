12b1e1b6cad7231025842fb9a20cfcb62932d3bd
aqlnnqib <aqlannonline@gmail.com>
2025-05-18 20:17:02 +0800

    Revert "item"
    This reverts commit 5d8b088f5b7328e4edf9966d69aae437d9b09e20, reversing
    changes made to 0a520c4f71bb510a88f610eb6f9481365a961a27.

--- player 2/scripts/player_2.gd
+++ player 2/scripts/player_2.gd
@@ -151,17 +151,8 @@ func _on_regen_timer_timeout():
 		if health <= 0:
 			health = 0
 			
-<<<<<<< HEAD
 			
 			
 		
 func player():
 	pass
-=======
-func _input(event):
-	if event.is_action_pressed("pickup"):
-		if $PickupZone.items_in_range.size() > 0:
-			var pickup_items = $PickupZone.items_in_range.values()[0]
-			pickup_items.pick_up_item(self)
-			$PickupZone.items_in_range.erase(pickup_items)
->>>>>>> 6867bcf622feb471c7a848b71978c17bbef93a7b
