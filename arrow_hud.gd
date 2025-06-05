67ee21d904b5530bf28fd62483839d7de9912a8f
EzraGGIt <ezra2766@gmail.com>
2025-06-05 19:42:32 +0800

    fix bug

--- arrow_hud.gd
+++ arrow_hud.gd
@@ -1,14 +1,6 @@
 extends CanvasLayer
 
-@onready var arrow_label := $Control/Label as Label  # More specific path
+@onready var arrow_label = $Control/Label
 
-func _ready():
-	# Wait until next frame to ensure all nodes are ready
-	await get_tree().process_frame
-	update_display(5)  # Initial value
-
-func update_display(count: int):
-	if arrow_label:  # Safety check
-		arrow_label.text = "Arrows: %d" % count
-	else:
-		push_error("Arrow Label not found! Check node path.")
+func update_arrow_count(count: int):
+	arrow_label.text = "Arrows: %d" % count
