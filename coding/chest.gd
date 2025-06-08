18b8f5dbbc1cc42e7c3fc0c218f78d4b2d87c6b9
Dairell <dairellhannan@yahoo.com>
2025-06-08 19:09:57 +0800

    chest fungsi

--- coding/chest.gd
+++ coding/chest.gd
@@ -5,9 +5,8 @@ var player_in_area = false
 
 var items = preload("res://SCENES/chest_collectable.tscn")
 
-@export var pickup_scene: PackedScene  
-@export var first_item: InvItem
-@export var sec_item: InvItem
+@export var item1: InvItem
+@export var item2: InvItem
 var player = null
 
 func _ready():
@@ -24,28 +23,29 @@ func _process(delta):
 	elif state == "open":
 		$AnimatedSprite2D.play("open")
 
-func drop_items():
-	await get_tree().create_timer(0.0).timeout
-	var first_item_instance = pickup_scene.instantiate()
-	first_item_instance.rotation = rotation
-	first_item_instance.global_position = $Marker2D.global_position
-	get_parent().add_child(first_item_instance)
-
-	
-	var sec_item_instance = pickup_scene.instantiate()
-	sec_item_instance.rotation = rotation
-	sec_item_instance.global_position = $Marker2D.global_position
-	get_parent().add_child(sec_item_instance)
-	
-
-
 func _on_pickable_area_body_entered(body: PhysicsBody2D):
 	if body.has_method("player"):
 		player_in_area = true
 		player = body
 
 
+
 func _on_pickable_area_body_exited(body: PhysicsBody2D):
 	if body.has_method("player"):
 		player_in_area = false
-		player = false
+	
+		
+func drop_items():
+	await get_tree().create_timer(0.0).timeout
+	var item1_instance = items.instantiate()
+	item1_instance.rotation = rotation
+	item1_instance.global_position = $Marker2D.global_position
+	get_parent().add_child(item1_instance)
+	player.collect(item1)
+	
+	await get_tree().create_timer(0.0).timeout
+	var item2_instance = items.instantiate()
+	item2_instance.rotation = rotation
+	item2_instance.global_position = $Marker2D.global_position
+	get_parent().add_child(item1_instance)
+	player.collect(item2)
