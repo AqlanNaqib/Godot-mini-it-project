b57676087d6f7e9ecf7f4fb933b50ff84bbb8c56
aqlnnqib <aqlannonline@gmail.com>
2025-06-09 22:36:18 +0800

    pause menu

---
+++ pause_menu.gd
@@ -0,0 +1,41 @@
+extends Control
+
+@onready var animation_player = $AnimationPlayer
+
+func _ready():
+	hide()  # Ensure hidden at start
+	# Optional: animation_player.play("RESET")
+
+func resume():
+	get_tree().paused = false
+	hide()
+	if animation_player.has_animation("blur"):
+		animation_player.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	show()
+	if animation_player.has_animation("blur"):
+		animation_player.play("blur")
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
+		if visible:  # If menu is currently showing
+			resume()
+		else:
+			pause()
+		get_tree().root.set_input_as_handled()
+		
+
+# Button signals
+func _on_resume_pressed():
+	resume()
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _on_restart_pressed():
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+	
+	
