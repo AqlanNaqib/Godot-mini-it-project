78cce6935cdbe8117db4f75c45616c11e1d3789a
aqlnnqib <aqlannonline@gmail.com>
2025-06-15 21:58:30 +0800

    VICTORY SONG

--- victory.gd
+++ victory.gd
@@ -1,9 +1,12 @@
 extends CanvasLayer
 
+@onready var queen: AudioStreamPlayer = $queen
 
 
 
 
+func _ready():
+	queen.play()
 
 func _on_exit_pressed() -> void:
 	get_tree().change_scene_to_file("res://main menu/main_menu.tscn")
